08122da093e940ff0db0b30f4a3711b07d7de875
Nanagouda <nanasaheb2164@gmail.com>
2023-02-23 23:39:02 +0530

    add static POST and GET measurement endpoints

---
+++ config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    host: 'localhost',
+    port: '5432',
+    database: 'shweta_iot',
+    user: 'postgres',
+    password: 'nanu',
+    
+})
--- controllers/measureControllers.js
+++ controllers/measureControllers.js
@@ -5,6 +5,7 @@ exports.postMeasurement = function(req, res, next) {
     res.send(`The temperature is: ${temp}`);
 }
 
-exports.getLatestMeasurement - function(req, res, next) {
-    res.send('The latest measurement was')
+exports.getLatestMeasurement = function(req, res, next) {
+    res.send('The latest measurement was');
 }
+
